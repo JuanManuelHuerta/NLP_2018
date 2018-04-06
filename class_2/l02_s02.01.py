fd7582fc1342d4595057fea2d3727df39fc94d36
JuanMHuerta <jmhuertany@gmail.com>
2018-04-06 10:00:46 -0400

    Updates to first class

---
+++ class_2/l02_s02.01.py
@@ -0,0 +1,103 @@
+import sys
+import json
+import unicodedata
+import numpy
+#from nltk import sent_tokenize, word_tokenize, pos_tag
+import nltk
+import re
+import operator
+
+def custom_word_tokenize(my_string):
+	s0=my_string
+	s1=re.sub(r'([a-z])\.([A-Z])',r'\1 \2',s0)
+	s2=re.sub(r'[;\,\:\s \(\)\-\!\?]',r' ',s1.lower())
+	s3=re.sub(r'\. ',r' ',s2)
+	words=s3.split(" ")
+	return words
+
+
+
+fp=open("stop_words.txt","rt")
+stop_words=set()
+for line in fp:
+	stop_words.add(line.rstrip())
+print "Loaded stop words!"
+
+
+
+fp=open("../reviews_Automotive_5.json","rt")
+all_data = []
+reviews_text=[]
+products_count={}
+
+for line in fp:
+	review_data = json.loads(line)
+	all_data.append(review_data)
+	review_n = unicodedata.normalize('NFKD',review_data["reviewText"]).encode('ascii','ignore')
+	reviews_text.append(review_n)
+	asin = review_data["asin"]
+	if not asin in products_count:
+		products_count[asin]=0
+	products_count[asin]+=1
+
+
+print "Number of reviews", len(all_data)
+print "Number of products", len(products_count)
+print "Average review per product", numpy.mean(products_count.values())
+
+#for review in reviews_text:
+#	print "Original Review", review
+#	print "nltk tokenizer", word_tokenize(review)
+#	print "custom tokenizer", custom_word_tokenize(review)
+
+
+master_dictionary={}
+dictionary_per_score={}
+words_per_score={}
+
+for review_data in all_data:
+	review = unicodedata.normalize('NFKD',review_data["reviewText"]).encode('ascii','ignore')
+	score = review_data["overall"]
+	if not score in dictionary_per_score:
+		dictionary_per_score[score]={}
+
+	if not score in words_per_score:
+		words_per_score[score]=0.0
+
+	#words = custom_word_tokenize(review)
+	words = nltk.word_tokenize(review)
+	words_pos = nltk.pos_tag(words)
+
+
+	for c_word in words_pos:
+		word=c_word[0].lower()
+		
+		#if c_word[1]=='NN':
+		if c_word[1]=='JJ':
+			if not word in master_dictionary:
+				master_dictionary[word]=0
+			if not word in dictionary_per_score[score]:
+				dictionary_per_score[score][word]=0
+			master_dictionary[word]+=1
+			dictionary_per_score[score][word]+=1
+			words_per_score[score]+=1.0
+
+
+for score in dictionary_per_score:
+	for word in dictionary_per_score[score]:
+		dictionary_per_score[score][word]/=words_per_score[score]
+
+for score in dictionary_per_score:
+	print "Score", score
+	dps_sorted = sorted(dictionary_per_score[score].items(),key=operator.itemgetter(1),reverse=True)
+	for word in dps_sorted:
+		print score, word
+
+
+
+
+
+
+
+
+
---
+++ class_2/l02_s05.01.py
@@ -0,0 +1,119 @@
+import sys
+import json
+import unicodedata
+import numpy
+from nltk import sent_tokenize, word_tokenize, pos_tag
+import re
+import operator
+import math
+
+def custom_word_tokenize(my_string):
+	s0=my_string
+	s1=re.sub(r'([a-z])\.([A-Z])',r'\1 \2',s0)
+	s2=re.sub(r'[;\,\:\s \(\)\-\!\?]',r' ',s1.lower())
+	s3=re.sub(r'\. ',r' ',s2)
+	words=s3.split(" ")
+	return words
+
+
+
+fp=open("stop_words.txt","rt")
+stop_words=set()
+for line in fp:
+	stop_words.add(line.rstrip())
+print "Loaded stop words!"
+
+
+
+fp=open("../reviews_Automotive_5.json","rt")
+all_data = []
+reviews_text=[]
+products_count={}
+
+for line in fp:
+	review_data = json.loads(line)
+	all_data.append(review_data)
+	review_n = unicodedata.normalize('NFKD',review_data["reviewText"]).encode('ascii','ignore')
+	reviews_text.append(review_n)
+	asin = review_data["asin"]
+	if not asin in products_count:
+		products_count[asin]=0
+	products_count[asin]+=1
+
+
+#print "Number of reviews", len(all_data)
+#print "Number of products", len(products_count)
+#print "Average review per product", numpy.mean(products_count.values())
+
+#for review in reviews_text:
+#	print "Original Review", review
+#	print "nltk tokenizer", word_tokenize(review)
+#	print "custom tokenizer", custom_word_tokenize(review)
+
+
+master_dictionary={}
+dictionary_per_score={}
+
+
+for review_data in all_data:
+	review = unicodedata.normalize('NFKD',review_data["reviewText"]).encode('ascii','ignore')
+	score = review_data["asin"]
+	if not score in dictionary_per_score:
+		dictionary_per_score[score]={}
+	words = custom_word_tokenize(review)
+	for word in words:
+		if not word in stop_words:
+			if not word in master_dictionary:
+				master_dictionary[word]=0
+			if not word in dictionary_per_score[score]:
+				dictionary_per_score[score][word]=0
+			master_dictionary[word]+=1
+			dictionary_per_score[score][word]+=1
+
+
+top_words={}
+InverseDocumentFrequency={}
+
+for asin in dictionary_per_score:
+	for word in dictionary_per_score[asin]:
+		if not word  in InverseDocumentFrequency:
+			InverseDocumentFrequency[word]=0
+		InverseDocumentFrequency[word]+=1
+
+inverse_index={}
+
+for score in dictionary_per_score:
+	print "ASIN", score
+
+	for word in dictionary_per_score[score]:
+		dictionary_per_score[score][word]=dictionary_per_score[score][word]*math.log(len(dictionary_per_score)/InverseDocumentFrequency[word])
+
+		if not word in inverse_index:
+			inverse_index[word]={}
+
+		inverse_index[word][score]=dictionary_per_score[score][word]
+
+while True:
+	query = raw_input("Juan's Google Killer > ").rstrip().split()
+	results={}
+	for word in query:
+		if word in inverse_index:
+			for asin in inverse_index[word]:
+				if not asin in results:
+					results[asin]=0.0
+				results[asin]+=inverse_index[word][asin]
+	sr=sorted(results.items(),key=operator.itemgetter(1),reverse=True)
+	if len(sr)>10:
+		sr=sr[:10]
+	print sr
+
+
+
+
+
+
+
+
+
+
+
